d772bb62f410d436116d0200bdb37fedd8ad8d77
WWdev85 <58261535+WWdev85@users.noreply.github.com>
2021-02-08 15:56:56 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import StoreProvider from './store/storeProvider';
 
 import './App.scss';
-import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
+
 import { Switch , HashRouter as Router, Route} from 'react-router-dom';
-import Hangman from './components/Hangman/Hangman';
-import Saper from './components/Saper/Saper'
+
 
 const App = () => ( 
     <StoreProvider>
-        <Header />
+        
         <Router>
+        <div>  sadasd  </div>
             <Switch>
-                <Route exact path="/" render={() => <Menu />} />
-                <Route path="/wisielec" render={() => <Hangman />} />
-                <Route path="/saper" render={() => <Saper />} />
+               
             </Switch>
         </Router>
     </StoreProvider>
